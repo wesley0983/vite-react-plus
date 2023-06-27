334ce14916f3b38189e9006a94f05a56043fd774
wesley.lsw <wesley.lsw@link8tech.tw>
2023-06-27 18:01:24 +0800

    番茄鐘位置調整

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '/src/style/home/index.scss'
 import SideBar from '../side-bar/index'
 
 export default () => {
+  const [second, setSecond] = useState(0);
+  const [hour, setHour] = useState(1500);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const handleCountdown = () => {
+    console.log('wesley function called');
+    if (intervalId) {
+      // 計時器已經在運行中，不需要重複啟動
+      return;
+    }
+    handleSecond()
+  };
+
+  const handleSecond = () => {
+    const clockTimer = setInterval(() => {
+      // decrease time left / increase time spent
+      setHour(preCount => preCount -1);
+    }, 1000);
+    console.log(clockTimer)
+  }
+
+  const stopTimer = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  };
+
 
   return (
     <>
       <div className="container">
         <div className="home">
           <div className="left">
-
+            <div className="time">
+              {hour}
+            </div>
+            <div className="bg">
+              <div className="circle">
+                <div className="countdown" onClick={handleCountdown}/>
+              </div>
+            </div>
           </div>
           <div className="right">
-            <SideBar />
           </div>
+          <SideBar />
         </div>
       </div>
     </>
--- src/components/side-bar/index.jsx
+++ src/components/side-bar/index.jsx
@@ -8,11 +8,14 @@ export default () => {
             <img src="src/assets/list/1x/baseline_list_white_24dp.png" alt=""/>
           </div>
           <div className="side__menu__item">
-            <img src="src/assets/list/1x/baseline_list_white_24dp.png" alt=""/>
+            <img src="src/assets/chart/1x/baseline_insert_chart_outlined_white_24dp.png" alt=""/>
           </div>
           <div className="side__menu__item">
-            <img src="src/assets/list/1x/baseline_list_white_24dp.png" alt=""/>
+            <img src="src/assets/music/1x/baseline_library_music_white_24dp.png" alt=""/>
           </div>
+        </div>
+        <div className="side__logo">
+          POMODORO
         </div>
       </div>
     </>
